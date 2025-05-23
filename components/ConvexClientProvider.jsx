458c1520d5dd3447bd8db0affeda1f0cd22a3cca
SushAN766 <sushanthbs766@gmail.com>
2025-05-23 17:50:41 +0530

    Storing user in DB

--- components/ConvexClientProvider.jsx
+++ components/ConvexClientProvider.jsx
@@ -1,18 +1,19 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-
+import { ConvexReactClient } from "convex/react";
+import { useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }) {
   return (
-    <ConvexProvider
+    <ConvexProviderWithClerk
      client={convex}
-     //useAuth={}
+     useAuth={useAuth}
      >
 
       {children}
-    </ConvexProvider>
+    </ConvexProviderWithClerk>
   );
 }
