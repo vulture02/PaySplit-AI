b89948a9877d9613866f09568652d492fb7c493a
SushAN766 <sushanthbs766@gmail.com>
2025-05-23 22:09:52 +0530

    database design completion

--- convex/schema.js
+++ convex/schema.js
@@ -1,5 +1,6 @@
 import { defineSchema , defineTable} from 'convex/server';
 import { v } from "convex/values";
+
 export default defineSchema({
     users: defineTable({
         name: v.string(),
@@ -12,4 +13,56 @@ export default defineSchema({
     .searchIndex("search_name", { searchField: "name" })
     .searchIndex("search_email", { searchField: "email" }),
     
+    // Expenses
+    expenses: defineTable({
+        description: v.string(),
+        amount: v.number(),
+        category: v.optional(v.string()),
+        date: v.number(), 
+        paidByUserId: v.id("users"), 
+        splitType: v.string(), 
+        splits: v.array(
+            v.object({
+                userId: v.id("users"), 
+                amount: v.number(), 
+                paid: v.boolean(),
+            })
+        ),
+        groupId: v.optional(v.id("groups")), 
+        createdBy: v.id("users"), 
+    })
+    .index("by_group", ["groupId"])
+    .index("by_user_and_group", ["paidByUserId", "groupId"])
+    .index("by_date", ["date"]),
+    
+
+    // Groups
+    groups: defineTable({
+     name: v.string(),
+     description: v.optional(v.string()),
+     createdBy: v.id("users"), 
+     members: v.array(
+      v.object({
+        userId: v.id("users"), 
+        role: v.string(), 
+        joinedAt: v.number(),
+      })
+    ),
+  }),
+
+  // Settlements
+  settlements: defineTable({
+    amount: v.number(),
+    note: v.optional(v.string()),
+    date: v.number(), // timestamp
+    paidByUserId: v.id("users"), // Reference to users table
+    receivedByUserId: v.id("users"), // Reference to users table
+    groupId: v.optional(v.id("groups")), // null for one-on-one settlements
+    relatedExpenseIds: v.optional(v.array(v.id("expenses"))), // Which expenses this settlement covers
+    createdBy: v.id("users"), // Reference to users table
+  })
+    .index("by_group", ["groupId"])
+    .index("by_user_and_group", ["paidByUserId", "groupId"])
+    .index("by_receiver_and_group", ["receivedByUserId", "groupId"])
+    .index("by_date", ["date"]),
 });
