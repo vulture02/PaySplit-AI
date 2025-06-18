2647117918f32b7d405cecfbe23a679f0340374e
SushAN766 <sushanthbs766@gmail.com>
2025-06-18 18:51:14 +0530

    feat: Add expenses page

---
+++ app/(main)/expenses/new/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+import { useRouter } from "next/navigation";
+
+const NewExpensePage = () => {
+  const router = useRouter();
+
+  return <div>NewExpensePage</div>
+  
+};
+
+export default NewExpensePage
